b7660faccfd2e79343f35ebc16f5716d7b028f93
Owen Nelson <onelson@gmail.com>
2020-04-12 21:55:55 -0700

    Derive a parser from pest grammar.
    Following the lead of the pest README.md, we derive a Parser on a unit
    struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,68 @@
+#[macro_use]
+extern crate pest_derive;
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "erd.pest"]
+struct ErdParser;
+
 #[cfg(test)]
 mod tests {
+    use super::{ErdParser, Parser, Rule};
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_parse_single_entity() {
+        let input = r#"
+        [Person]
+            *name
+            height
+            weight
+            `birth date`
+            +birth_place_id
+        "#;
+        ErdParser::parse(Rule::erd, input).unwrap();
+    }
+
+    #[test]
+    fn test_parse_double_entity() {
+        let input = r#"
+        [Person]
+            *name
+            height
+            weight
+            `birth date`
+            +birth_place_id
+
+        [`Birth Place`]
+            *id
+            `birth city`
+            'birth state'
+            "birth country"
+        "#;
+        ErdParser::parse(Rule::erd, input).unwrap();
+    }
+    #[test]
+    fn test_parse_double_entity_with_cardinality() {
+        let input = r#"
+        [Person]
+            *name
+            height
+            weight
+            `birth date`
+            +birth_place_id
+
+        [`Birth Place`]
+            *id
+            `birth city`
+            'birth state'
+            "birth country"
+        # Cardinality    Syntax
+        # 0 or 1         ?
+        # exactly 1      1
+        # 0 or more      *
+        # 1 or more      +
+        Person *--1 `Birth Place`
+        "#;
+        ErdParser::parse(Rule::erd, input).unwrap();
     }
 }
