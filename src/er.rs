1f634f531c1e9eb41a10d7bdb44719413e8e1a72
Owen Nelson <onelson@gmail.com>
2020-05-24 14:58:02 -0700

    Lots and lots of empty stub functions, some notes on pending type refactors.

--- src/er.rs
+++ src/er.rs
@@ -7,7 +7,63 @@
 //! language representation of this).
 
 use crate::Result;
+use std::cmp::Ordering;
 use std::collections::hash_map::{Entry, HashMap};
+use std::fmt::{Display, Formatter};
+
+/// Represents a single schema.
+#[derive(Debug, PartialEq)]
+pub struct ER {
+    entities: Vec<Entity>,
+    rels: Vec<Relation>,
+    title: Options,
+}
+
+/// Represents a single entity in a schema.
+#[derive(Debug, Eq, PartialEq)]
+pub struct Entity {
+    name: String,
+    attribs: Vec<Attribute>,
+    /// Formatting options for the header.
+    hoptions: Options,
+    /// Formatting options for the entity "body."
+    eoptions: Options,
+}
+
+/// Default ordering for `Entity` (by name).
+impl Ord for Entity {
+    fn cmp(&self, _other: &Self) -> Ordering {
+        unimplemented!()
+    }
+}
+/// Default ordering for `Entity` (by name).
+impl PartialOrd for Entity {
+    fn partial_cmp(&self, _other: &Self) -> Option<Ordering> {
+        unimplemented!()
+    }
+}
+
+/// Represents an attribute on a particular entity.
+#[derive(Debug, Eq, PartialEq)]
+pub struct Attribute {
+    field: String,
+    pk: bool,
+    fk: bool,
+    options: Options,
+}
+
+/// Default ordering for `Attribute` (by field name).
+impl Ord for Attribute {
+    fn cmp(&self, other: &Self) -> Ordering {
+        unimplemented!()
+    }
+}
+/// Default ordering for `Attribute` (by field name).
+impl PartialOrd for Attribute {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        unimplemented!()
+    }
+}
 
 /// The haskell version refers to this as "global options" in the parser but
 /// also "directives."
@@ -17,19 +73,44 @@ use std::collections::hash_map::{Entry, HashMap};
 /// various object types in the graph.
 pub type GlobalOptions = HashMap<Directive, Options>;
 
+/// Used as a key for the [GlobalOptions](type.GlobalOptions.html) type.
+#[derive(Debug, PartialEq)]
+pub enum Directive {
+    Title,
+    Header,
+    Entity,
+    Relationship,
+}
+
+/// A collection of formatting options.
+#[derive(Debug)]
 // FIXME:
-//  Seems like the `Options`/`FormatOption` type might be all wrong. We need specific key
-//  names matched against specific value types. This sounds like a struct
-//  to me, but a struct would not give us the filter-ability needed to run
-//  selectors over without a common type for the values.
+//  Seems like the `Options`/`FormatOption` type might be all wrong.
+//  We need specific key names matched against specific value types.
+//  This sounds like a struct to me, but a struct would not give us the
+//  filter-ability needed to run selectors over without a common type for the
+//  values.
 //  Perhaps we need to change `FormatOptions` to carry a typed value in each
 //  variant. Each variant would correspond to a string name. The `Options` type
 //  would then become `HashMap<String, FormatOption>` (if still relevant).
-/// A collection of formatting options.
-pub type Options = HashMap<FormatOption, String>;
+pub struct Options(HashMap<FormatOption, String>);
+
+impl Eq for Options {
+    fn assert_receiver_is_total_eq(&self) {
+        unimplemented!()
+    }
+}
+
+impl PartialEq for Options {
+    fn eq(&self, other: &Self) -> bool {
+        unimplemented!()
+    }
+}
+
 type OptEntry<'m> = Entry<'m, FormatOption, String>;
 
 /// Used as a key in the [Options](type.Options.html) type.
+#[derive(Debug, PartialEq)]
 pub enum FormatOption {
     BgColor,
     Color,
@@ -43,54 +124,16 @@ pub enum FormatOption {
     TextAlignment,
 }
 
-pub struct Attribute {
-    field: String,
-    pk: bool,
-    fk: bool,
-    options: Options,
-}
-
-/// Defined at each side of a [Relation](struct.Relation.html) a cardinality
-/// describes the count constraints for each entity.
-pub enum Cardinality {
-    ZeroOne,
-    One,
-    ZeroPlus,
-    OnePlus,
-}
-
-/// Used as a key for the [GlobalOptions](type.GlobalOptions.html) type.
-pub enum Directive {
-    Title,
-    Header,
-    Entity,
-    Relationship,
-}
-
-pub struct Entity {
-    name: String,
-    attribs: Vec<Attribute>,
-    options: Options,
-}
-
-pub struct Relation {
-    entity1: String,
-    entity2: String,
-    card1: Cardinality,
-    card2: Cardinality,
-    options: Options,
-}
-
 /// Given two sets of options, merge the second into first, where elements
 /// in the first take precedence.
-fn merge_opts(_: Options, _: Options) -> Options {
+fn merge_opts(_: &Options, _: &Options) -> Options {
     unimplemented!()
 }
 
 /// Given a set of options and a selector function, return the list of
 /// only those options which matched. Examples of the selector function are
 /// `opt_to_font`, `opt_to_html` and `opt_to_label`.
-fn options_to<'a, F>(_: &'a Options, _: F) -> Options
+fn options_to<'a, F>(_selector: F, _options: &'a Options) -> Options
 where
     F: Fn(&OptEntry<'a>) -> Option<&'a OptEntry<'a>>,
 {
@@ -101,7 +144,7 @@ where
 /// `option_by_name` will attempt to parse the string as a value corresponding
 /// to the option. If the option doesn't exist or there was a problem parsing
 /// the value, an error is returned.
-fn option_by_name(_: &str, _: &str) -> Result<()> {
+fn option_by_name(_name: &str, _value: &str) -> Result<()> {
     // FIXME: need a way to unify the return type.
     //  `FormatOption` with an inner value?
     unimplemented!()
@@ -130,5 +173,59 @@ fn opt_to_html(_opt: &FormatOption) -> Option<&FormatOption> {
 
 /// Selects an option if and only if it corresponds to a label.
 fn opt_to_label(_opt: &FormatOption) -> Option<&FormatOption> {
+    unimplemented!()
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Relation {
+    entity1: String,
+    entity2: String,
+    card1: Cardinality,
+    card2: Cardinality,
+    options: Options,
+}
+
+/// Defined at each side of a [Relation](struct.Relation.html) a cardinality
+/// describes the count constraints for each entity.
+#[derive(Debug, PartialEq)]
+pub enum Cardinality {
+    ZeroOne,
+    One,
+    ZeroPlus,
+    OnePlus,
+}
+
+impl Display for Cardinality {
+    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
+        unimplemented!()
+    }
+}
+
+fn card_by_name(_: char) -> Option<Cardinality> {
+    unimplemented!()
+}
+
+/// Hard-coded default options for all graph titles.
+fn default_title_opts() -> Options {
+    unimplemented!()
+}
+
+/// Hard-coded default options for all entity headers.
+fn default_header_opts() -> Options {
+    unimplemented!()
+}
+
+/// Hard-coded default options for all entities.
+fn default_entity_opts() -> Options {
+    unimplemented!()
+}
+
+/// Hard-coded default options for all relationships.
+fn default_rel_opts() -> Options {
+    unimplemented!()
+}
+
+/// Hard-coded default options for all attributes.
+fn default_attr_opts() -> Options {
     unimplemented!()
 }
