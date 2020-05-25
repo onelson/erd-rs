f584033abb8bed106eedb5a2d77216980256ce56
Owen Nelson <onelson@gmail.com>
2020-05-24 17:24:18 -0700

    Refactor types around the formatting options.
    Seems like the option values should be an enum with inner values of an
    appropriate type for each case. Part of what's tricky here is the
    original Haskell used types in each case that aligned with the graphviz
    lib they had, but since we're targeting dot lang as our output *and* the
    dot lib we have expects us to prepare what boils down to html strings
    using these as attributes, all these options are effectively "string" if
    we want to align with html attributes...
    
    I think we should type the options a little more strictly so that we can
    provide better feedback to the caller before handing our dot lang over to the
    dot cli.
    
    So, for now, the intent will be to parse the raw strings from our ER
    file, then cast them back to string for our html output. Okay.

--- src/er.rs
+++ src/er.rs
@@ -54,13 +54,13 @@ pub struct Attribute {
 
 /// Default ordering for `Attribute` (by field name).
 impl Ord for Attribute {
-    fn cmp(&self, other: &Self) -> Ordering {
+    fn cmp(&self, _other: &Self) -> Ordering {
         unimplemented!()
     }
 }
 /// Default ordering for `Attribute` (by field name).
 impl PartialOrd for Attribute {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    fn partial_cmp(&self, _other: &Self) -> Option<Ordering> {
         unimplemented!()
     }
 }
@@ -68,10 +68,16 @@ impl PartialOrd for Attribute {
 /// The haskell version refers to this as "global options" in the parser but
 /// also "directives."
 ///
-/// Formatting options can be specified for each of these directive types, and
-/// the options set will provide a fallback for each when rendering the
-/// various object types in the graph.
-pub type GlobalOptions = HashMap<Directive, Options>;
+/// Formatting options can be specified for each of these directive types in the
+/// header section of the er file.
+/// The options will provide a fallback for each when rendering the various
+/// object types in the graph.
+pub struct GlobalOptions {
+    title: Options,
+    header: Options,
+    entity: Options,
+    relationship: Options,
+}
 
 /// Used as a key for the [GlobalOptions](type.GlobalOptions.html) type.
 #[derive(Debug, PartialEq)]
@@ -85,15 +91,15 @@ pub enum Directive {
 /// A collection of formatting options.
 #[derive(Debug)]
 // FIXME:
-//  Seems like the `Options`/`FormatOption` type might be all wrong.
+//  Seems like the `Options`/`Opt` type might be all wrong.
 //  We need specific key names matched against specific value types.
 //  This sounds like a struct to me, but a struct would not give us the
 //  filter-ability needed to run selectors over without a common type for the
 //  values.
-//  Perhaps we need to change `FormatOptions` to carry a typed value in each
+//  Perhaps we need to change `Opt`s to carry a typed value in each
 //  variant. Each variant would correspond to a string name. The `Options` type
-//  would then become `HashMap<String, FormatOption>` (if still relevant).
-pub struct Options(HashMap<FormatOption, String>);
+//  would then become `HashMap<String, Opt>` (if still relevant).
+pub struct Options(HashMap<String, Opt>);
 
 impl Eq for Options {
     fn assert_receiver_is_total_eq(&self) {
@@ -102,26 +108,63 @@ impl Eq for Options {
 }
 
 impl PartialEq for Options {
-    fn eq(&self, other: &Self) -> bool {
+    fn eq(&self, _other: &Self) -> bool {
         unimplemented!()
     }
 }
 
-type OptEntry<'m> = Entry<'m, FormatOption, String>;
+type OptEntry<'m> = Entry<'m, Opt, String>;
+
+// The following type aliases are stubs matching the Haskell types (mostly).
+// In many cases, the types used to represent these formatting options are
+// selected based on the graphviz api being used, but for us we have no such
+// wrapper to align with. *Practically speaking* we could represent all these
+// values as String and just pass them straight through in our dot output.
+//
+// This may in fact be what we need to do.
+
+// Seems like this is going to be any valid hex color, or one of the named
+// colors available in html.
+// <https://graphviz.gitlab.io/_pages/doc/info/colors.html>
+type Color = String;
+type Text = String;
+type Double = f64;
+type Word8 = u8;
+// FIXME: change to an enum?
+//  Described in <https://www.graphviz.org/doc/info/shapes.html#html> as:
+//  > specifies horizontal placement. When an object is allocated more space
+//  > than required, this value determines where the extra space is placed left
+//  > and right of the object.
+//  >  - CENTER aligns the object in the center. (Default)
+//  >  - LEFT aligns the object on the left.
+//  >  - RIGHT aligns the object on the right.
+//  >  - (<TD> only) TEXT aligns lines of text using the full cell width. The
+//       alignment of a line is determined by its (possibly implicit) associated
+//       <BR> element.
+//  >
+//  > The contents of a cell are normally aligned as a block.
+//  > In particular, lines of text are first aligned as a text block based on
+//  > the width of the widest line and the corresponding <BR> elements.
+//  > Then, the entire text block is aligned within a cell. If, however, the
+//  > cell's ALIGN value is "TEXT", and the cell contains lines of text, then
+//  > the lines are justified using the entire available width of the cell. If
+//  > the cell does not contain text, then the contained image or table is
+//  > centered.
+type Align = String;
 
 /// Used as a key in the [Options](type.Options.html) type.
 #[derive(Debug, PartialEq)]
-pub enum FormatOption {
-    BgColor,
-    Color,
-    FontFace,
-    FontSize,
-    Border,
-    BorderColor,
-    CellSpacing,
-    CellBorder,
-    CellPadding,
-    TextAlignment,
+pub enum Opt {
+    BgColor(Color),
+    Color(Color),
+    FontFace(Text),
+    FontSize(Double),
+    Border(Word8),
+    BorderColor(Color),
+    CellSpacing(Word8),
+    CellBorder(Word8),
+    CellPadding(Word8),
+    TextAlignment(Align),
 }
 
 /// Given two sets of options, merge the second into first, where elements
@@ -144,9 +187,7 @@ where
 /// `option_by_name` will attempt to parse the string as a value corresponding
 /// to the option. If the option doesn't exist or there was a problem parsing
 /// the value, an error is returned.
-fn option_by_name(_name: &str, _value: &str) -> Result<()> {
-    // FIXME: need a way to unify the return type.
-    //  `FormatOption` with an inner value?
+fn option_by_name(_name: &str, _value: &str) -> Result<Opt> {
     unimplemented!()
 }
 
@@ -161,18 +202,18 @@ fn option_parse() -> Result<()> {
 }
 
 /// Selects an option if and only if it corresponds to a font attribute.
-fn opt_to_font(_opt: &FormatOption) -> Option<&FormatOption> {
+fn opt_to_font(_opt: &Opt) -> Option<&Opt> {
     unimplemented!()
 }
 
 /// Selects an option if and only if it corresponds to an HTML attribute.
 /// In particular, for tables or table cells.
-fn opt_to_html(_opt: &FormatOption) -> Option<&FormatOption> {
+fn opt_to_html(_opt: &Opt) -> Option<&Opt> {
     unimplemented!()
 }
 
 /// Selects an option if and only if it corresponds to a label.
-fn opt_to_label(_opt: &FormatOption) -> Option<&FormatOption> {
+fn opt_to_label(_opt: &Opt) -> Option<&Opt> {
     unimplemented!()
 }
 
