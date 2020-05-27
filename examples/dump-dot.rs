1b41900d77f0dd39bc3bfd3f63f66a834280b215
Owen Nelson <onelson@gmail.com>
2020-05-27 15:11:16 -0700

    Generate dot language for rich types. No support for options yet.

---
+++ examples/dump-dot.rs
@@ -0,0 +1,14 @@
+//! Little program that reads an ERD file and spits out the tree of nodes that
+//! have been parsed.
+
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    let fp = std::env::args().nth(1).unwrap();
+    let mut erd_file = File::open(&fp).unwrap();
+    let mut buf = String::new();
+    erd_file.read_to_string(&mut buf).unwrap();
+    let er = erd_rs::er::parse(&buf).unwrap();
+    println!("{}", er);
+}
--- src/render.rs
+++ src/render.rs
@@ -3,7 +3,65 @@
 //!
 //! <https://www.graphviz.org/doc/info/command.html>
 use crate::er::{Attribute, Entity, Options, Relation, ER};
+use std::fmt::{Display, Formatter};
 
 trait AsDot {
     fn as_dot(&self) -> String;
+}
+
+impl AsDot for Attribute {
+    fn as_dot(&self) -> String {
+        format!("<tr><td>{}</td></tr>", self.field)
+    }
+}
+
+impl AsDot for Entity {
+    fn as_dot(&self) -> String {
+        let attrs = {
+            let xs: Vec<_> = self.attribs.iter().map(|attr| attr.as_dot()).collect();
+            xs.join("\n")
+        };
+        format!(
+            r#""{0}" [label=<
+        <table>
+            <tr><td>{0}</td></tr>
+            {1}
+        </table>>];"#,
+            self.name, attrs
+        )
+    }
+}
+
+impl AsDot for ER {
+    fn as_dot(&self) -> String {
+        let entities: Vec<_> = self.entities.iter().map(|e| e.as_dot()).collect();
+        let relationships: Vec<_> = self.rels.iter().map(|e| e.as_dot()).collect();
+
+        format!(
+            r#"
+        digraph ER {{
+            {0}
+            {1}
+        }}
+        "#,
+            entities.join("\n"),
+            relationships.join("\n")
+        )
+    }
+}
+
+impl AsDot for Relation {
+    fn as_dot(&self) -> String {
+        // note that the arrow *starts at the tail* and *ends at the head*.
+        format!(
+            r#""{0}" -> "{1}" [taillabel="{2}", headlabel="{3}"];"#,
+            self.entity1, self.entity2, self.card1, self.card2,
+        )
+    }
+}
+
+impl Display for ER {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_dot())
+    }
 }
